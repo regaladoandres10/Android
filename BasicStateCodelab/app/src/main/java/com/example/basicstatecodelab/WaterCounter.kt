004d1ef9f1c9279ebb01171683148684461f2a21
regaladoandres10 <alunaregalado@gmail.com>
2025-10-05 18:30:22 -0600

    Agregando el composable WellnessTaskItem nos permite ver las tareas y ademas agregando recomposicion al composable WaterCounter

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -17,15 +18,34 @@ import androidx.compose.runtime.setValue
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = Modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text(
-            text = "You've had $count glasses.",
-            modifier = Modifier.padding(16.dp)
-        )
-        Button(
-            onClick = {count++},
-            modifier = modifier.padding(top = 8.dp)
-        ) {
-            Text("Add one")
+        if(count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if(showTask) {
+                WellnessTaskItem(
+                    //Cambiando el estado de showTask
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            Text(
+                text = "You've had $count glasses.",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+        Row (modifier = Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = {count++},
+                //modifier = modifier.padding(top = 8.dp),
+                enabled = count < 10
+            ) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 
