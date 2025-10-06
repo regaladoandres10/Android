5fb8fe88d052c9e36f0facae283f38a5cfde7189
regaladoandres10 <alunaregalado@gmail.com>
2025-10-05 22:55:46 -0600

    Agregando la elevacion de estado dentro del componente StatefulCounter, esto para poder utilizar el composable en diferentes composables

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,7 +1,6 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -12,41 +11,24 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
-        if(count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if(showTask) {
-                WellnessTaskItem(
-                    //Cambiando el estado de showTask
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
-            Text(
-                text = "You've had $count glasses.",
-                modifier = Modifier.padding(16.dp)
-            )
+fun StatelessCounter( count:Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
         }
-        Row (modifier = Modifier.padding(top = 8.dp)) {
-            Button(
-                onClick = {count++},
-                //modifier = modifier.padding(top = 8.dp),
-                enabled = count < 10
-            ) {
-                Text("Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 8.dp)
-            ) {
-                Text("Clear water count")
-            }
+        Button(onClick = { onIncrement }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
+}
 
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    //Mandamos llamara el composable para modificar sus valores.
+    StatelessCounter(count, {count++}, modifier)
 }
