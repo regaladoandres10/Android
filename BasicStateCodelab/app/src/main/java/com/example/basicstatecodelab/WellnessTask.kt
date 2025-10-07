15e0addb65b68ad1897cea82d46d208ad5241772
regaladoandres10 <alunaregalado@gmail.com>
2025-10-07 11:28:45 -0600

    Terminando el proyecto, agregamos el estado ViewModel

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessTask.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessTask.kt
@@ -1,12 +1,19 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 /**
  * Esta clase nos permite obtener el id de la tarea (id)
  * Asi como descripción (label)
  */
-data class WellnessTask(
+class WellnessTask(
     val id: Int,
     val label: String,
-){
-
+    //Almacenar el estado de marcado
+    initialChecked: Boolean = false,
+) {
+    var checked by mutableStateOf(initialChecked)
 }
