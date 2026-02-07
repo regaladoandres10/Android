dfc9e98bf8f96841e6531953dd3c8379dd2814b4
regaladoandres10 <alunaregalado@gmail.com>
2026-02-06 22:13:44 -0600

    Agregar proyecto de WorkManager

---
+++ basic-android-kotlin-compose-training-workmanager/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -0,0 +1,60 @@
+package com.example.bluromatic.workers
+
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
+import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+private const val TAG = "BlurWorker"
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+    override suspend fun doWork(): Result {
+
+        makeStatusNotification(
+            applicationContext.resources.getString(R.string.blurring_image),
+            applicationContext
+        )
+
+        return withContext(Dispatchers.IO) {
+            return@withContext try {
+
+                //Delay o retraso entre los mensajes de notificacion
+                delay(DELAY_TIME_MILLIS)
+
+                val picture = BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.drawable.android_cupcake
+                )
+
+                val output = blurBitmap(picture, 1)
+
+                // Write bitmap to a temp file
+                val outputUri = writeBitmapToFile(applicationContext, output)
+
+                //mostrar al usuario un mensaje de notificación
+                //Show message notification
+                makeStatusNotification(
+                    "Output is $outputUri",
+                    applicationContext
+                )
+
+                Result.success()
+            } catch (throwable: Throwable) {
+                Log.e(
+                    TAG,
+                    applicationContext.resources.getString(R.string.error_applying_blur),
+                    throwable
+                )
+                Result.failure()
+            }
+        }
+
+
+    }
+}
