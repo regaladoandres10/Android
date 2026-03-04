cd5edb3a76d4bb994ecb3f73ca7bcf4d547654ef
regaladoandres10 <alunaregalado@gmail.com>
2026-03-03 22:36:19 -0600

    Agregando al Mapper el toEntity de cardex para pasar de Cardex a CardexEntity

--- AppSice/app/src/main/java/com/example/appsice/data/local/entity/Mapper.kt
+++ AppSice/app/src/main/java/com/example/appsice/data/local/entity/Mapper.kt
@@ -1,5 +1,6 @@
 package com.example.appsice.data.local.entity
 
+import com.example.appsice.data.remote.model.Cardex
 import com.example.appsice.data.remote.model.CargaAcademica
 import com.example.appsice.data.remote.model.ProfileStudent
 import kotlinx.serialization.InternalSerializationApi
@@ -43,5 +44,24 @@ fun CargaAcademica.toEntity(): CargaAcademicaEntity {
         creditosMateria = this.creditosMateria ?: 0,
         materia = this.materia ?: "",
         grupo = this.grupo ?: ""
+    )
+}
+
+@OptIn(InternalSerializationApi::class)
+fun Cardex.toEntity(): CardexEntity {
+    return CardexEntity(
+        fecEsp = this.fecEsp ?: "",
+        clvMat = this.clvMat ?: "",
+        clvOfiMat = this.clvOfiMat ?: "",
+        materia = this.materia ?: "",
+        cdts = this.cdts ?: 0,
+        calif = this.calif ?: 0,
+        acred = this.acred ?: "",
+        s1 = this.s1 ?: "",
+        p1 = this.p1 ?: "",
+        a1 = this.a1 ?: "",
+        s2 = this.s2 ?: "",
+        p2 = this.p2 ?: "",
+        a2 = this.a2 ?: ""
     )
 }
