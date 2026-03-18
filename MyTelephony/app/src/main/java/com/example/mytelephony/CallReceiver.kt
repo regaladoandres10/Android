25da8fc28b90103cdce6a1962b7f3a0222c53cee
regaladoandres10 <alunaregalado@gmail.com>
2026-03-18 15:04:06 -0600

    Implementando el BroadcastReceiver

---
+++ MyTelephony/app/src/main/java/com/example/mytelephony/CallReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.mytelephony
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class CallReceiver: BroadcastReceiver() {
+    //Metodo principal para recibir eventos en el BroadcastReceiver
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+
+}
