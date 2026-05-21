7674e913f2cda09ee9fc48341af595148410a02e
regaladoandres10 <alunaregalado@gmail.com>
2026-05-21 16:20:37 -0600

    Agregando la api para hacer la peticiones http con ktorfit

---
+++ AppSiceKMP/shared/src/commonMain/kotlin/data/remote/api/SICENETService.kt
@@ -0,0 +1,60 @@
+package data.remote.api
+
+import de.jensklingenberg.ktorfit.http.Body
+import de.jensklingenberg.ktorfit.http.Headers
+import de.jensklingenberg.ktorfit.http.POST
+
+interface SICENETService {
+    // Login
+    @Headers(
+        "Content-Type: text/xml;charset=utf-8",
+        "SOAPAction: http://tempuri.org/accesoLogin"
+    )
+    @POST("/ws/wsalumnos.asmx")
+    suspend fun acceso(@Body soap: String): String
+
+
+    // Perfil
+    @Headers(
+        "Content-Type: text/xml;charset=utf-8",
+        "SOAPAction: http://tempuri.org/getAlumnoAcademicoWithLineamiento"
+    )
+    @POST("/ws/wsalumnos.asmx")
+    suspend fun profile(@Body soap: String): String
+
+
+    // Carga académica
+    @Headers(
+        "Content-Type: text/xml;charset=utf-8",
+        "SOAPAction: http://tempuri.org/getCargaAcademicaByAlumno"
+    )
+    @POST("/ws/wsalumnos.asmx")
+    suspend fun cargaAcademica(@Body soap: String): String
+
+
+    // Cardex
+    @Headers(
+        "Content-Type: text/xml;charset=utf-8",
+        "SOAPAction: http://tempuri.org/getAllKardexConPromedioByAlumno"
+    )
+    @POST("/ws/wsalumnos.asmx")
+    suspend fun getKardex(@Body soap: String): String
+
+
+    // Calificaciones unidad
+    @Headers(
+        "Content-Type: text/xml;charset=utf-8",
+        "SOAPAction: http://tempuri.org/getCalifUnidadesByAlumno"
+    )
+    @POST("/ws/wsalumnos.asmx")
+    suspend fun getCaliPorUnidad(@Body soap: String): String
+
+
+    // Calificación final
+    @Headers(
+        "Content-Type: text/xml;charset=utf-8",
+        "SOAPAction: http://tempuri.org/getAllCalifFinalByAlumnos"
+    )
+    @POST("/ws/wsalumnos.asmx")
+    suspend fun getCaliFinal(@Body soap: String): String
+}
