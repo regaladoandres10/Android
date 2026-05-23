79facf2994cd811179491b9f459fbe24708e89c3
regaladoandres10 <alunaregalado@gmail.com>
2026-05-23 15:03:16 -0600

    Creando el composable principal para la navegación con navigation

--- AppSiceKMP/shared/src/commonMain/kotlin/ui/viewmodel/SNUiState.kt
+++ AppSiceKMP/shared/src/commonMain/kotlin/ui/viewmodel/SNUiState.kt
@@ -1,4 +1,4 @@
-package presentation
+package ui.viewmodel
 
 sealed interface SNUiState {
 
--- AppSiceKMP/shared/src/commonMain/kotlin/ui/viewmodel/SNViewModel.kt
+++ AppSiceKMP/shared/src/commonMain/kotlin/ui/viewmodel/SNViewModel.kt
@@ -1,4 +1,4 @@
-package presentation
+package ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
